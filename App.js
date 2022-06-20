0705e1d806953a2542260da1c613c05bd3fd1d78
Martin <martin.alvarado2002@gmail.com>
2022-06-20 11:19:46 +0200

    Initial Container setup

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+} from 'react-native';
 
 export default function App() {
-    return <View style={styles.container}></View>;
+    return (
+        <SafeAreaView style={styles.app}>
+            <View style={styles.app__search}>
+                <Text> Search </Text>
+            </View>
+            <View style={styles.app__list}>
+                <Text> List </Text>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    app: {
         flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+    app__search: {
+        padding: 10,
+    },
+    app__list: {
+        flex: 1,
+        padding: 10,
     },
 });
