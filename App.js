5e14b8074a11ea0b8c570aef0da2fbb4576a98cd
Martin <martin.alvarado2002@gmail.com>
2022-06-22 17:49:15 +0200

    [#2]Configured ESLint

--- App.js
+++ App.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    Platform,
-    StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
 } from 'react-native';
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.app}>
-            <View style={styles.app__search}>
-                <Text> Search </Text>
-            </View>
-            <View style={styles.app__list}>
-                <Text> List </Text>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.app}>
+      <View style={styles.app__search}>
+        <Text> Search </Text>
+      </View>
+      <View style={styles.app__list}>
+        <Text> List </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    app: {
-        flex: 1,
-        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    },
-    app__search: {
-        padding: 10,
-    },
-    app__list: {
-        flex: 1,
-        padding: 10,
-    },
+  app: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  app__search: {
+    padding: 10,
+  },
+  app__list: {
+    flex: 1,
+    padding: 10,
+  },
 });
